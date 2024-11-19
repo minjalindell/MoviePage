2c2d4eee4d0cf3a08c701631187509a00a9c72ec
lohielisabeth <ellipetti01@hotmail.com>
2024-11-19 13:13:04 +0200

    hakua kehitetty

---
+++ src/components/profile.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './profile.css'; // Luo tarvittaessa oma CSS tiedosto tyylejä varten.
+
+function Profile() {
+  return (
+    <div className="profile-container">
+      <header className="profile-header">
+        <h1>Your Profile</h1>
+      </header>
+      <nav className="profile-nav">
+        <Link to="/search">
+          <button className="profile-button">Search movies</button>
+        </Link>
+        <Link to="/shows">
+          <button className="profile-button">Search shows</button>
+        </Link>
+      </nav>
+      {/* Lisää muita profiilikohtia täällä, kuten suosikit, arvostelut, jne. */}
+    </div>
+  );
+}
+
+export default Profile;
