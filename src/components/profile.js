71df8760b4646496a180993428f4dbfd5275a7a1
lohielisabeth <ellipetti01@hotmail.com>
2024-11-20 10:42:15 +0200

    hakua kehitetty

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './profile.css'; // Luo tarvittaessa oma CSS tiedosto tyylejä varten.
+import './profile.css'; 
 
 function Profile() {
   return (
